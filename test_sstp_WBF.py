b53cd4781ab870be66260728268f069e1a3373e2
AgnieszkaMakulska <agnieszka300911@gmail.com>
2026-03-04 14:10:56 +0100

    WBF test

--- test_sstp_WBF.py
+++ test_sstp_WBF.py
@@ -14,9 +14,9 @@ from functions import rh_to_rh_i
 from libcloudphxx import common
 from matplotlib.ticker import MaxNLocator
 
-sstp_list = [10]
+sstp = 10
 w_max = 1.
-z_max = 2000.
+z_max = 1500.
 
 aerosol = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 0.071e-6], "gstdev": [1.36, 1.57], "n_tot": [160.0e6, 380.0e6]},' \
                 '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
@@ -31,9 +31,9 @@ def run_scheme(outfile, mixing, sstp):
         dt=1,
         z_max=z_max,
         w = 1.,
-        #z_max=None,
-        #w=lambda t: w_max * np.pi / 2. * np.sin(np.pi*t*w_max/z_max),
-        #t = 2. * z_max / w_max,
+        # z_max=None,
+        # w=lambda t: w_max * np.pi / 2. * np.sin(np.pi*t*w_max/z_max),
+        # t = 2. * z_max / w_max,
         outfile=outfile,
         outfreq=10,
         scheme="lgrngn",
@@ -63,10 +63,9 @@ def run_scheme(outfile, mixing, sstp):
         liq_conc = np.array(f.variables['liq_m0'][:]).squeeze()  # 1/kg
         ice_conc = np.array(f.variables['ice_m0'][:]).squeeze()  # 1/kg
         act_conc = np.array(f.variables['act_m0'][:]).squeeze()
-        liq_r = np.where(liq_conc > 0, np.array(f.variables['liq_m1'][:]).squeeze() / np.array(f.variables['liq_m0'][:]).squeeze(), 0)
         ice_r = np.where(ice_conc > 0, np.array(f.variables['ice_m1'][:]).squeeze() / np.array(f.variables['ice_m0'][:]).squeeze(), 0)
         act_r = np.where(act_conc > 0, np.array(f.variables['act_m1'][:]).squeeze() / np.array(f.variables['act_m0'][:]).squeeze(), 0)
-    return RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, liq_conc, ice_conc, act_conc, liq_r, ice_r, act_r
+    return RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r
 
 
 
@@ -74,29 +73,30 @@ def make_figure(sstp):
 
     fig, ax = plt.subplots(1, 5, figsize=(15.0, 8.0), sharey=True, squeeze=False)
 
-    for mixing in [True]:
+    for mixing in [True, False]:
 
         outfile = f"test_sstp_WBF_mix_{mixing}.nc"
-        RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, liq_conc, ice_conc, act_conc, liq_r, ice_r, act_r = run_scheme(outfile, mixing, sstp)
+        RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r = run_scheme(outfile, mixing, sstp)            
 
-        (ice_c, liq_c, ice_l, liq_l) = ("skyblue", "coral", "ice (with mixing)", "liquid (with mixing)") if mixing else ("steelblue", "sienna", "ice (without mixing)", "liquid (without mixing)")
+        (ice_c, liq_c, ice_l, liq_l) = ("skyblue", "coral", "ice (coupled)", "liquid (coupled)") if mixing else ("steelblue", "sienna", "ice (uncoupled)", "liquid (uncoupled)")
+        (c, l) = ("violet", "coupled") if mixing else ("purple", "uncoupled")
 
-        ax[0,0].plot(ice_mix_ratio * 1e3, z, color=ice_c)
-        ax[0,0].plot(liq_mix_ratio * 1e3, z, color=liq_c)
+        ax[0,0].plot(ice_mix_ratio * 1e3, z, color=ice_c, label=ice_l)
+        ax[0,0].plot(liq_mix_ratio * 1e3, z, color=liq_c, label=liq_l)
         ax[0,1].plot(ice_conc / 1e6, z, label=ice_l, color=ice_c)
         ax[0,1].plot(act_conc / 1e6, z, label=liq_l, color=liq_c)
         ax[0,2].plot(ice_r * 1e6, z, color=ice_c)
         ax[0,2].plot(act_r * 1e6, z, color=liq_c)
+        ax[0,3].plot((rv + liq_mix_ratio + ice_mix_ratio) * 1e3, z, color=c, label=l)
         ax[0,4].plot([rh_to_rh_i(RH_val, T_val) for (RH_val, T_val) in zip(RH, T) ], z, color=ice_c)
         ax[0,4].plot(RH, z, color=liq_c)
 
-        (c,l) = ("violet", "mixing") if mixing == True else ("purple", "no mixing")
-        ax[0,3].plot((rv + liq_mix_ratio + ice_mix_ratio) * 1e3, z, color=c, label=l)
-        ax[0,3].legend(loc="center")
+    ax[0,3].legend()
+    handles, labels = ax[0,0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.15, 0.2))
 
-    ax[0,1].legend(loc='center')
-    ax[0,0].set_xlabel('mix ratio [g/kg]')
     ax[0,0].set_ylabel('z [m]')
+    ax[0,0].set_xlabel('mix ratio [g/kg]')
     ax[0,1].set_xlabel('concentration [1/mg]')
     ax[0,2].set_xlabel("average radius [um]")
     ax[0,3].set_xlabel("total mix ratio [g/kg]")
@@ -112,8 +112,5 @@ def make_figure(sstp):
 
     return fig
 
-
-for sstp in sstp_list:
-    make_figure(sstp)
-
+make_figure(sstp)
 plt.show()
