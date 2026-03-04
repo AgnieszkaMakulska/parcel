b2ce6c4384eaea4fa491b4cf81087b09b353f7e3
AgnieszkaMakulska <agnieszka300911@gmail.com>
2026-03-04 19:42:46 +0100

    different w, aerosols

--- test_sstp_WBF.py
+++ test_sstp_WBF.py
@@ -15,13 +15,16 @@ from libcloudphxx import common
 from matplotlib.ticker import MaxNLocator
 
 sstp = 10
-w_max = 1.
+w_list = [0.5, 1., 2.5]
 z_max = 1500.
 
-aerosol = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 0.071e-6], "gstdev": [1.36, 1.57], "n_tot": [160.0e6, 380.0e6]},' \
+polluted = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 0.071e-6], "gstdev": [1.36, 1.57], "n_tot": [160.0e6, 380.0e6]},' \
                 '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
 
-def run_scheme(outfile, mixing, per_particle):
+pristine = '{"pristine": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6, 0.06e-6], "gstdev": [1.2, 1.7], "n_tot": [125.0e6, 65.0e6]},' \
+                '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
+
+def run_scheme(outfile, mixing, per_particle, aerosol, w_max):
     args = dict(
         p_0=100000,
         RH_0=0.9,
@@ -32,8 +35,9 @@ def run_scheme(outfile, mixing, per_particle):
         # z_max=z_max,
         # w = 1.,
         z_max=None,
-        w=lambda t: w_max * np.pi / 2. * np.sin(np.pi*t*w_max/z_max),
-        t = z_max / w_max,
+        #w=lambda t: w_max * np.pi / 2. * np.sin(np.pi*t*w_max/z_max),
+        w = lambda t: w_max if t <= z_max/w_max else -w_max,
+        t = 2 * z_max / w_max,
         outfile=outfile,
         outfreq=10,
         scheme="lgrngn",
@@ -68,14 +72,14 @@ def run_scheme(outfile, mixing, per_particle):
 
 
 
-def make_figure():
+def make_figure(aerosol, w_max):
 
     fig, ax = plt.subplots(1, 5, figsize=(15.0, 8.0), sharey=True, squeeze=False)
 
     for (mixing, per_particle) in [(True, True), (False, True), (True, False)]:
 
         outfile = f"test_WBF.nc"
-        RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r = run_scheme(outfile, mixing, per_particle)            
+        RH, T, rv, z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r = run_scheme(outfile, mixing, per_particle, aerosol, w_max)            
 
         if not per_particle:
             (ice_c, liq_c, ice_l, liq_l) = ("dimgrey", "dimgrey", "ice (per-cell)", "liquid (per-cell)")
@@ -96,7 +100,8 @@ def make_figure():
         ax[0,1].plot(act_conc / 1e6, z, label=liq_l, color=liq_c, linestyle=s)
         ax[0,2].plot(ice_r * 1e6, z, color=ice_c, linestyle=s)
         ax[0,2].plot(act_r * 1e6, z, color=liq_c, linestyle=s)
-        ax[0,3].plot((rv + liq_mix_ratio + ice_mix_ratio) * 1e3, z, color=c, label=l, linestyle=s)
+        #ax[0,3].plot((rv + liq_mix_ratio + ice_mix_ratio) * 1e3, z, color=c, label=l, linestyle=s)
+        ax[0,3].plot(T, z, color=c, label=l, linestyle=s)
         ax[0,4].plot([rh_to_rh_i(RH_val, T_val) for (RH_val, T_val) in zip(RH, T) ], z, color=ice_c, linestyle=s)
         ax[0,4].plot(RH, z, color=liq_c, linestyle=s)
 
@@ -108,18 +113,22 @@ def make_figure():
     ax[0,0].set_xlabel('mix ratio [g/kg]')
     ax[0,1].set_xlabel('concentration [1/mg]')
     ax[0,2].set_xlabel("average radius [um]")
-    ax[0,3].set_xlabel("total mix ratio [g/kg]")
+    #ax[0,3].set_xlabel("total mix ratio [g/kg]")
+    ax[0,3].set_xlabel("T [K]")
     ax[0,4].set_xlabel("RH")
 
-    ax[0,3].ticklabel_format(style='plain', useOffset=False)
-    ax[0,3].xaxis.set_major_locator(MaxNLocator(4))
+    #ax[0,3].ticklabel_format(style='plain', useOffset=False)
+    #ax[0,3].xaxis.set_major_locator(MaxNLocator(4))
 
     fig.tight_layout(rect=(0, 0.10, 1, 0.97))
-    out_png = "test_WBF_mixing.png"
-    plt.suptitle('WBF process with ' + str(sstp) + ' substeps')
+    aerosol_str = "pristine" if aerosol==pristine else "polluted"
+    out_png = "test_WBF_w_"+str(w_max)+"_"+aerosol_str+".pdf"
+    plt.suptitle('WBF process with '+str(sstp)+' substeps, w = '+str(w_max)+' m/s, '+aerosol_str)
     plt.savefig(out_png, dpi=200)
 
     return fig
 
-make_figure()
-plt.show()
+for w_max in w_list:
+    for aerosol in [pristine, polluted]:
+        make_figure(aerosol, w_max)
+#plt.show()
