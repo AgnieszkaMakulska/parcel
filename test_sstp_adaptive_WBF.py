6d2d43c1297ec7bf1d50300a0d3a45069b8d1b00
AgnieszkaMakulska <agnieszka300911@gmail.com>
2026-03-26 15:49:45 +0100

    add walltime

--- test_sstp_adaptive_WBF.py
+++ test_sstp_adaptive_WBF.py
@@ -10,13 +10,8 @@ import numpy as np
 from parcel import parcel
 from scipy.io import netcdf
 import matplotlib.pyplot as plt
-from functions import rh_to_rh_i
 from libcloudphxx import common
-from matplotlib.ticker import MaxNLocator
-import matplotlib.colors as mcolors
-from matplotlib.collections import LineCollection
 
-sstp_max = 10
 w_list = [1., 2.5, 5.]
 z_max_list = [1250., 2000, 3000.]
 
@@ -26,7 +21,7 @@ polluted = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 
 pristine = '{"pristine": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6, 0.06e-6], "gstdev": [1.2, 1.7], "n_tot": [125.0e6, 65.0e6]},' \
                 '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
 
-def run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act):
+def run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond):
     args = dict(
         p_0=100000,
         RH_0=0.9,
@@ -43,7 +38,7 @@ def run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act):
         scheme="lgrngn",
         out_bin='{"liq": {"rght": 1, "moms": [0,1,2,3], "drwt": "wet", "nbin": 1, "lnli": "lin", "left": 0.5e-20},' \
                 '"ice": {"rght": 1, "moms": [0,1,2,3], "drwt": "ice_a", "nbin": 1, "lnli": "lin", "left": 0.5e-20}}',
-        sstp_cond = sstp_max,
+        sstp_cond = sstp_cond,
         adaptive_sstp_cond = adaptive,
         sstp_cond_mix   = False,
         exact_sstp_cond = True,
@@ -84,35 +79,38 @@ def run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act):
         std_dev_ice = np.sqrt(np.where(ice_m0 > 0, 
                            ice_m2 / ice_m0 - (ice_m1 / ice_m0)**2, 
                            0))
+        step_cond_walltime_ms = np.array(f.variables['step_cond_walltime_ms'][:]).squeeze()
 
-    return z/1000, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, sstp_cond_mean, sstp_dep_mean, std_dev_liq*1e6, std_dev_ice*1e6
+    return z/1000, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, sstp_cond_mean, sstp_dep_mean, std_dev_liq*1e6, std_dev_ice*1e6, step_cond_walltime_ms
 
 
 
 def make_figure(aerosol, w_max, z_max):
 
-    fig, ax = plt.subplots(2, 5, figsize=(12.0, 10.0), sharey=True, squeeze=True)
+    fig, ax = plt.subplots(2, 5, figsize=(13.0, 10.0), sharey=True, squeeze=True)
 
-    for adaptive, epsilon, sstp_act in [(True, 1e-1, None),(True, 1e-2, None), (True, 1e-3, None), (False, None, None), (True, None, 10)]:
+    for adaptive, epsilon, sstp_act, sstp_cond in [(True, 1e-1, None, 10),(True, 1e-2, None, 10), (True, 1e-3, None, 10), (False, None, None, 10), (True, None, 10, 1)]:
 
         outfile = f"test_WBF.nc"
-        z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, sstp_cond_mean, sstp_dep_mean, variance_liq, variance_ice = run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act)           
+        z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, sstp_cond_mean, sstp_dep_mean, std_dev_liq, std_dev_ice, step_cond_walltime_ms = run_scheme(outfile, aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond)           
+
+        walltime_mean = np.nanmean(step_cond_walltime_ms)
 
         if not adaptive:
             c = 'grey'
             s = ':'
-            l = 'non-adaptive'
+            l = 'non-adaptive'+', t = '+str(round(walltime_mean,1))+' ms'
         elif sstp_act == 10:
-            c = 'green'
+            c = 'darkorange'
             s = ':'
-            l = 'sstp_act = 10'
+            l = 'sstp_act = 10'+', t = '+str(round(walltime_mean,1))+' ms'
         else:
             s = '-'
-            l = '$\epsilon$ = '+str(epsilon)
+            l = '$\epsilon$ = '+str(epsilon)+', t = '+str(round(walltime_mean,1))+' ms'
             if epsilon == 1e-3:
                 c = 'blue'
             elif epsilon == 1e-2:
-                c = 'purple'
+                c = 'seagreen'
             else:
                 c = 'violet'
 
@@ -122,8 +120,8 @@ def make_figure(aerosol, w_max, z_max):
         ax[0,1].plot(act_conc, z, color=c, label=l, linestyle=s)
         ax[1,2].plot(ice_r, z, color=c, linestyle=s)
         ax[0,2].plot(act_r, z, color=c, linestyle=s)
-        ax[1,3].plot(variance_ice, z, color=c, label=l, linestyle=s)
-        ax[0,3].plot(variance_liq, z, color=c, label=l, linestyle=s)
+        ax[1,3].plot(std_dev_ice, z, color=c, label=l, linestyle=s)
+        ax[0,3].plot(std_dev_liq, z, color=c, label=l, linestyle=s)
         if adaptive:
             ax[1,4].plot(sstp_dep_mean, z, color=c, linestyle=s)
             ax[0,4].plot(sstp_cond_mean, z, color=c, linestyle=s)
@@ -133,7 +131,7 @@ def make_figure(aerosol, w_max, z_max):
                           (liq_mix_ratio, ax[0,0]), (ice_mix_ratio, ax[1,0]),
                           (act_conc, ax[0,1]), (ice_conc, ax[1,1]),
                           (act_r, ax[0,2]), (ice_r, ax[1,2]),
-                          (variance_liq, ax[0,3]), (variance_ice, ax[1,3]),
+                          (std_dev_liq, ax[0,3]), (std_dev_ice, ax[1,3]),
                           (sstp_cond_mean, ax[0,4]), (sstp_dep_mean, ax[1,4])
                            ]:
             if not adaptive and (var is sstp_cond_mean or var is sstp_dep_mean):
@@ -151,18 +149,14 @@ def make_figure(aerosol, w_max, z_max):
             )
             axis.annotate(
                 '',
-                xy=(var[3*len(var)//4+8], z[3*len(z)//4+8]),    # Grot strzałki
-                xytext=(var[3*len(var)//4+6], z[3*len(z)//4+6]), # Początek strzałki
+                xy=(var[3*len(var)//5+8], z[3*len(z)//5+8]),    # Grot strzałki
+                xytext=(var[3*len(var)//5+6], z[3*len(z)//5+6]), # Początek strzałki
                 arrowprops=dict(
                     arrowstyle='simple',
                     color=c,
                     linewidth=2
                 )
             )
-
-    ax[0,0].legend()
-    #handles, labels = ax[0,0].get_legend_handles_labels()
-    #fig.legend(handles, labels, loc="lower center", bbox_to_anchor=(0.15, 0.2))
 
     ax[0,0].set_ylabel('z [km]')
     ax[1,0].set_ylabel('z [km]')
@@ -180,7 +174,9 @@ def make_figure(aerosol, w_max, z_max):
     ax[0,4].set_xlim(0,10.5)
     ax[1,4].set_xlim(0,10.5)
 
-    fig.tight_layout(rect=[0, 0, 1, 0.95])
+    handles, labels = ax[0,0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="center right", bbox_to_anchor=(0.16, 0.5))
+    fig.tight_layout(rect=[0.15, 0, 1, 0.95])
     aerosol_str = "pristine" if aerosol==pristine else "polluted"
     out_png = "test_adaptive_WBF_w_"+str(w_max)+"_"+aerosol_str+".png"
     plt.suptitle('w = '+str(w_max)+' m/s, '+aerosol_str)
