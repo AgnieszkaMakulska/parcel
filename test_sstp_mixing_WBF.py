4965e70ff7dc3f0c83c1c212fc52723eb1e605b7
AgnieszkaMakulska <agnieszka300911@gmail.com>
2026-04-14 16:13:18 +0200

    better plots for coupled/uncoupled

--- test_sstp_mixing_WBF.py
+++ test_sstp_mixing_WBF.py
@@ -1,5 +1,5 @@
 """
-Testing adaptive timesteps with the WBF process
+Checking if mixing between substeps is important for the WBF process
 """
 
 import sys, os
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 from libcloudphxx import common
 plt.rcParams.update({'font.size': 14})
 
+sstp = 10
 w_list = [1., 2.5, 5.]
 z_max_list = [1250., 2000, 3000.]
 outfile = f"test_WBF.nc"
@@ -23,7 +24,7 @@ polluted = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 
 pristine = '{"pristine": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6, 0.06e-6], "gstdev": [1.2, 1.7], "n_tot": [125.0e6, 65.0e6]},' \
                 '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
 
-def run_scheme(aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond):
+def run_scheme(mixing, per_particle, aerosol, w_max, z_max):
     args = dict(
         p_0=100000,
         RH_0=0.9,
@@ -39,19 +40,16 @@ def run_scheme(aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond):
         scheme="lgrngn",
         out_bin='{"liq": {"rght": 1, "moms": [0,1,2,3], "drwt": "wet", "nbin": 1, "lnli": "lin", "left": 0.5e-20},' \
                 '"ice": {"rght": 1, "moms": [0,1,2,3], "drwt": "ice_a", "nbin": 1, "lnli": "lin", "left": 0.5e-20}}',
-        sstp_cond = sstp_cond,
-        adaptive_sstp_cond = adaptive,
-        sstp_cond_mix   = False,
-        exact_sstp_cond = True,
+        sstp_cond = sstp,
+        adaptive_sstp_cond = False,
+        sstp_cond_mix   = mixing,
+        exact_sstp_cond = per_particle,
         aerosol_independent_of_rhod=True, 
         backend="OpenMP",
         ice_switch = True,
         ice_nucl = True,
         time_dep_ice_nucl = False,
-        depo = True,
-        sstp_cond_adapt_drw2_eps = epsilon,
-        sstp_cond_act = sstp_act
-        #sstp_cond_adapt_drw2_max=100,
+        depo = True
     )
 
     parcel(**args)
@@ -64,60 +62,48 @@ def run_scheme(aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond):
         ice_m0 = np.array(f.variables['ice_m0'][:]).squeeze()
         ice_m1 = np.array(f.variables['ice_m1'][:]).squeeze()
         ice_m2 = np.array(f.variables['ice_m2'][:]).squeeze()
-
         ice_mix_ratio = np.array(f.variables['ice_mix_ratio'][:]).squeeze()
         liq_mix_ratio = np.array(f.variables['liq_m3'][:]).squeeze() * 4/3 * np.pi * common.rho_w
         ice_conc = np.array(f.variables['ice_m0'][:]).squeeze()  # 1/kg
         act_conc = np.array(f.variables['act_m0'][:]).squeeze()
         ice_r = np.where(ice_conc > 0, np.array(f.variables['ice_m1'][:]).squeeze() / np.array(f.variables['ice_m0'][:]).squeeze(), 0)
         act_r = np.where(act_conc > 0, np.array(f.variables['act_m1'][:]).squeeze() / np.array(f.variables['act_m0'][:]).squeeze(), 0)
-        sstp_cond_mean = np.array(f.variables['sstp_cond_mean'][:]) if 'sstp_cond_mean' in f.variables else np.zeros(z.shape)
-        sstp_dep_mean = np.array(f.variables['sstp_dep_mean'][:]) if 'sstp_dep_mean' in f.variables else np.zeros(z.shape)
-
         std_dev_liq = np.sqrt(np.where(act_m0 > 0, 
                            act_m2 / act_m0 - (act_m1 / act_m0)**2, 
                            0))
         std_dev_ice = np.sqrt(np.where(ice_m0 > 0, 
                            ice_m2 / ice_m0 - (ice_m1 / ice_m0)**2, 
                            0))
-        step_cond_walltime_ms = np.array(f.variables['step_cond_walltime_ms'][:]).squeeze()
-
     os.remove(outfile)
-    return z/1000, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, sstp_cond_mean, sstp_dep_mean, std_dev_liq*1e6, std_dev_ice*1e6, step_cond_walltime_ms
+    return z/1000, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, std_dev_liq*1e6, std_dev_ice*1e6
 
 
 
 def make_figure(aerosol, w_max, z_max):
 
-    fig, ax = plt.subplots(2, 5, figsize=(17.5, 10.0), sharey=True, squeeze=True)
+    fig, ax = plt.subplots(2, 4, figsize=(15, 10.0), sharey=True, squeeze=True)
 
-    for adaptive, epsilon, sstp_act, sstp_cond in [(False, None, None, 10), (True, None, 10, 1), (True, 1e-1, None, 10),(True, 1e-2, None, 10), (True, 1e-3, None, 10)]:
+    for (mixing, per_particle) in [(True, False), (True, True), (False, True)]:
 
-        z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, sstp_cond_mean, sstp_dep_mean, std_dev_liq, std_dev_ice, step_cond_walltime_ms = run_scheme(aerosol, w_max, z_max, adaptive, epsilon, sstp_act, sstp_cond)           
+        z, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, std_dev_liq, std_dev_ice = run_scheme(mixing, per_particle, aerosol, w_max, z_max)            
 
-        walltime_mean = np.nanmean(step_cond_walltime_ms)
-
-        if not adaptive:
+        if not per_particle:
             c = 'darkgrey'
             s = '-'
-            l = '10 substeps, '+str(round(walltime_mean,1))+' ms'
+            l = 'per-cell'
             lw = 3
-        elif sstp_act == 10:
-            c = 'gold'
+
+        if per_particle:
             s = ':'
-            l = '10 activation substeps, '+str(round(walltime_mean,1))+' ms'
             lw = 2
-        else:
-            s = ':'
-            l = 'adaptive, $\epsilon$ = '+str(epsilon)+', '+str(round(walltime_mean,1))+' ms'
-            lw = 2
-            if epsilon == 1e-3:
+            if mixing:
+                l = 'per-particle coupled'
                 c = 'blue'
-            elif epsilon == 1e-2:
-                c = 'seagreen'
             else:
+                l = 'per-particle uncoupled'
                 c = 'violet'
 
+        
         ax[1,0].plot(ice_mix_ratio, z, color=c, label=l, linestyle=s, linewidth = lw)
         ax[0,0].plot(liq_mix_ratio, z, color=c, label=l, linestyle=s, linewidth = lw)
         ax[1,1].plot(ice_conc, z, color=c, label=l, linestyle=s, linewidth = lw)
@@ -127,20 +113,13 @@ def make_figure(aerosol, w_max, z_max):
         ax[0,2].plot(act_r, z, color=c, linestyle=s, linewidth = lw)
         ax[1,3].plot(std_dev_ice, z, color=c, label=l, linestyle=s, linewidth = lw)
         ax[0,3].plot(std_dev_liq, z, color=c, label=l, linestyle=s, linewidth = lw)
-        if adaptive:
-            ax[1,4].plot(sstp_dep_mean, z, color=c, linestyle=s, linewidth = lw)
-            ax[0,4].plot(sstp_cond_mean, z, color=c, linestyle=s, linewidth = lw)
-
 
         for var, axis in [
                           (liq_mix_ratio, ax[0,0]), (ice_mix_ratio, ax[1,0]),
                           (act_conc, ax[0,1]), (ice_conc, ax[1,1]),
                           (act_r, ax[0,2]), (ice_r, ax[1,2]),
                           (std_dev_liq, ax[0,3]), (std_dev_ice, ax[1,3]),
-                          (sstp_cond_mean, ax[0,4]), (sstp_dep_mean, ax[1,4])
                            ]:
-            if not adaptive and (var is sstp_cond_mean or var is sstp_dep_mean):
-                continue
             
             axis.annotate(
                 '',
@@ -173,17 +152,12 @@ def make_figure(aerosol, w_max, z_max):
     ax[1,2].set_xlabel("ice mean radius [$\mu$m]")
     ax[0,3].set_xlabel('liquid std. deviation [$\mu$m]')
     ax[1,3].set_xlabel('ice std. deviation [$\mu$m]')
-    ax[0,4].set_xlabel("liquid sstp mean")
-    ax[1,4].set_xlabel("ice sstp mean")
-
-    ax[0,4].set_xlim(0,10.5)
-    ax[1,4].set_xlim(0,10.5)
 
     handles, labels = ax[0,0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc="center right", bbox_to_anchor=(0.22, 0.5))
-    fig.tight_layout(rect=[0.21, 0, 1, 0.95])
+    fig.legend(handles, labels, loc="center right", bbox_to_anchor=(0.21, 0.5))
+    fig.tight_layout(rect=[0.2, 0, 1, 0.95])
     aerosol_str = "pristine" if aerosol==pristine else "polluted"
-    out_png = "plots/outputs/test_adaptive_WBF_w_"+str(w_max)+"_"+aerosol_str+".svg"
+    out_png = "plots/outputs/test_WBF_mixing_w_"+str(w_max)+"_"+aerosol_str+".svg"
     plt.suptitle('w = '+str(w_max)+' m/s, '+aerosol_str)
     plt.savefig(out_png, dpi=200)
 
