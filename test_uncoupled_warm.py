64a69e6e26ca7afa20cad04d7b377e59243434e7
AgnieszkaMakulska <agnieszka300911@gmail.com>
2026-06-15 20:35:38 +0200

    updated coupling test

--- test_uncoupled_warm.py
+++ test_uncoupled_warm.py
@@ -11,20 +11,17 @@ from parcel import parcel
 from scipy.io import netcdf
 import matplotlib.pyplot as plt
 from libcloudphxx import common
-plt.rcParams.update({'font.size': 14})
+plt.rcParams.update({'font.size': 16})
 
-dt = 1
-sstp = 10
-w_list = [2.5]
-z_max_list = [1200] #1000 for w=0.2
+timesteps = [1, 3, 10]
+w_list = [0.25, 1., 4.]
+z_max_list = [2000]
 sd_conc = 100
 outfile = f"test_WBF.nc"
 
-polluted = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 0.071e-6], "gstdev": [1.36, 1.57], "n_tot": [160.0e6, 380.0e6]},' \
-                '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
+polluted = '{"polluted": {"kappa": 0.61, "rd_insol" : 0.0, "mean_r": [0.029e-6, 0.071e-6], "gstdev": [1.36, 1.57], "n_tot": [160.0e6, 380.0e6]}}'
 
-pristine = '{"pristine": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6, 0.06e-6], "gstdev": [1.2, 1.7], "n_tot": [125.0e6, 65.0e6]},' \
-                '"INP": {"kappa": 0.61, "rd_insol" : 0.5e-6, "mean_r": [0.029e-6], "gstdev": [1.36], "n_tot": [10.0e6]}}' # low concentration of INPs
+pristine = '{"pristine": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6, 0.06e-6], "gstdev": [1.2, 1.7], "n_tot": [125.0e6, 65.0e6]}}'
 
 monomod = '{"monomodal": {"kappa": 0.61, "rd_insol": 0.0, "mean_r": [0.011e-6], "gstdev": [1.2], "n_tot": [125.0e6]}}'
 
@@ -36,10 +33,10 @@ monodisperse = {
     }
 }
 
-aerosol_list = [pristine]
+aerosol_list = [pristine, polluted]
 
 
-def run_scheme(mixing, adaptive, aerosol, w_max, z_max):
+def run_scheme(mixing, dt, sstp, aerosol, w_max, z_max):
     args = dict(
         p_0=100000,
         RH_0=0.8,
@@ -57,7 +54,7 @@ def run_scheme(mixing, adaptive, aerosol, w_max, z_max):
         out_bin='{"liq": {"rght": 1, "moms": [0,1,2,3], "drwt": "wet", "nbin": 1, "lnli": "lin", "left": 0.5e-20},' \
                 '"ice": {"rght": 1, "moms": [0,1,2,3], "drwt": "ice_a", "nbin": 1, "lnli": "lin", "left": 0.5e-20}}',
         sstp_cond = sstp,
-        adaptive_sstp_cond = adaptive,
+        adaptive_sstp_cond = False,
         sstp_cond_mix   = mixing,
         exact_sstp_cond = True,
         aerosol_independent_of_rhod=True, 
@@ -96,63 +93,64 @@ def run_scheme(mixing, adaptive, aerosol, w_max, z_max):
                            0))
         sd_conc_liq = np.array(f.variables['sd_conc_liq'][:]).squeeze()
         sd_conc_ice = np.array(f.variables['sd_conc_ice'][:]).squeeze()
-    #os.remove(outfile)
-    return z/1000, rv*1e3, RH, th, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, std_dev_liq*1e6, std_dev_ice*1e6, sd_conc_liq, sd_conc_ice
+    os.remove(outfile)
+    return z/1000, rv*1e3, RH, th, ice_mix_ratio*1e3, liq_mix_ratio*1e3, ice_conc/1e6, act_conc/1e6, ice_r*1e6, act_r*1e6, std_dev_liq*1e6, std_dev_ice*1e6,
 
 
 
 def make_figure(aerosol, w_max, z_max):
 
-    fig, ax = plt.subplots(2, 4, figsize=(15, 10.0), sharey=True, squeeze=True)
+    fig, ax = plt.subplots(len(timesteps), 4, figsize=(16.0, 15.0), sharey=True, squeeze=True)
 
-    for (mixing, adaptive) in [(True, False), (False, False)]:
+    for i in range(len(timesteps)):
 
-        s = ':'
-        lw = 2
-        if mixing:
-            l = 'per-particle coupled'
-            c = 'blue'
-        else:
-            l = 'per-particle uncoupled'
-            c = 'violet'
-
-        z, rv, RH, th, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, std_dev_liq, std_dev_ice, sd_conc_liq, sd_conc_ice = run_scheme(mixing, adaptive, aerosol, w_max, z_max)            
-        r_tot = np.array(rv + liq_mix_ratio + ice_mix_ratio)
-
-
-        ax[0,0].plot(liq_mix_ratio, z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[0,1].plot(act_conc, z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[0,2].plot(act_r, z, color=c, linestyle=s, linewidth = lw)
-        ax[0,3].plot(std_dev_liq, z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[1,0].plot(rv, z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[1,1].plot(np.round(r_tot,1), z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[1,2].plot(th, z, color=c, label=l, linestyle=s, linewidth = lw)
-        ax[1,3].plot(RH, z, color=c, linestyle=s, linewidth = lw)
-        
-
+        dt = timesteps[i]
+        sstp = 10 * dt
     
-    ax[0,0].set_ylabel('z [km]')
-    ax[1,0].set_ylabel('z [km]')
+        for mixing in [True, False]:
 
-    ax[0,0].set_xlabel('liquid mixing ratio [g/kg]')
-    ax[0,1].set_xlabel('liquid conc. [1/mg]')
-    ax[0,2].set_xlabel(f'liquid mean radius [$\mu$m]')
-    ax[0,3].set_xlabel(f'liquid std dev')
-    ax[1,0].set_xlabel('rv [g/kg]')
-    ax[1,1].set_xlabel('r_tot [g/kg]')
-    ax[1,2].set_xlabel('th [K]')
-    ax[1,3].set_xlabel(f'RH')
+            lw = 2
+            if mixing:
+                l = 'coupled'
+                c = 'blue'
+                s = '-'
+            else:
+                l = 'uncoupled'
+                c = 'violet'
+                s = ':'
+
+            z, rv, RH, th, ice_mix_ratio, liq_mix_ratio, ice_conc, act_conc, ice_r, act_r, std_dev_liq, std_dev_ice = run_scheme(mixing, dt, sstp, aerosol, w_max, z_max)
+
+            ax[i,0].plot(liq_mix_ratio, z, color=c, label=l, linestyle=s, linewidth = lw)
+            ax[i,1].plot(act_conc, z, color=c, label=l, linestyle=s, linewidth = lw)
+            ax[i,2].plot(act_r, z, color=c, linestyle=s, linewidth = lw)
+            ax[i,3].plot(std_dev_liq, z, color=c, label=l, linestyle=s, linewidth = lw)
+    
+        ax[i,0].set_ylabel('z [km]')
+        ax[i,0].set_xlabel('liquid mixing ratio [g/kg]')
+        ax[i,1].set_xlabel('cloud droplet concentration [1/mg]')
+        ax[i,2].set_xlabel(f'cloud droplet mean radius [$\mu$m]')
+        ax[i,3].set_xlabel(f'cloud droplet std. dev. [$\mu$m]')
+
     handles, labels = ax[0,0].get_legend_handles_labels()
-    fig.legend(handles, labels, loc="center right", bbox_to_anchor=(0.21, 0.5))
-    fig.tight_layout(rect=[0.2, 0, 1, 0.95])
+    fig.legend(handles, labels, loc="center right", bbox_to_anchor=(0.8, 0.97))
+    fig.tight_layout(rect=[0, 0, 1, 0.95])
+
+    time_steps = ["dt = 1 s", "dt = 3 s", "dt = 10 s"]
+    for row_idx, label in enumerate(time_steps):
+        axis = ax[row_idx, 0]
+        axis.text(0.05, 0.95, label, transform=axis.transAxes, 
+                fontsize=16, fontweight='bold', va='top', ha='left',
+                bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'))
+
     if aerosol == pristine:
         aerosol_str = "pristine"
     elif aerosol == polluted:
         aerosol_str = "polluted"
     else:
         aerosol_str = "monomodal"
-    out_png = "plots/outputs/coupled_uncoupled/test_mixing_"+aerosol_str+"_dt_"+str(dt)+"_w_"+str(w_max)+".pdf"
-    plt.suptitle('w = '+str(w_max)+' m/s, dt = '+str(dt)+', '+str(sstp)+' substeps, '+ aerosol_str)
+    out_png = "plots/outputs/coupled_uncoupled/"+aerosol_str+"_w_"+str(w_max)+".pdf"
+    plt.suptitle('w = '+str(w_max)+' m/s, '+ aerosol_str)
     plt.savefig(out_png, dpi=200)
 
     return fig
